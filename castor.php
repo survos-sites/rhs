1f425cb90463f26cdc5f9182d613c9af3d75f921
Tac Tacelosky <tacman@gmail.com>
2026-02-19 10:42:42 -0500

    add cloud promo

--- castor.php
+++ castor.php
@@ -3,7 +3,6 @@
 use Castor\Attribute\AsTask;
 
 use function Castor\{io,run,capture,import};
-import('.castor/vendor/tacman/castor-tools/castor.php');
 
 import(__DIR__ . '/src/Command/CaFetchCommand.php');
 
